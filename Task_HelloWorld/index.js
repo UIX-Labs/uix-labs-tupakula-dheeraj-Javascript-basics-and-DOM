8184741c8f722d4540c853a98b6fab3af086abec
Frost bite <frostbite@Frosts-MacBook-Air.local>
2023-10-16 17:42:42 +0530

    Updated changes

---
+++ Task_HelloWorld/index.js
@@ -0,0 +1,26 @@
+// Print hello World
+// Write your code here
+
+// Write your code above
+
+//Declare a variable i and set its value to 5
+// Write your code here
+
+// Write your code above
+
+//Write the if else statement to console "Number is odd" if the value of i is odd and "Number is even" if the value of i is even.
+// Write your code here
+
+// Write your code above
+
+//Declare a string variable which is always constant and set is value to 'triangle'.
+// Write your code here
+
+// Write your code above
+
+//Write a function 'findSide' which takes an argument 'type' and return the number of sides in that shape
+// Write your code here
+
+// Write your code above
+const side = findSide('Circle');
+console.log('Side', side);
