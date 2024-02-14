35ddfdc9c14a67e6d5e97ef05747b5db417a4bea
tupakula dheeraj <deerajtupakula@gmail.com>
2024-02-14 18:15:21 +0530

    task: Project Task-Adding a event listener

--- Slambook/index.js
+++ Slambook/index.js
@@ -1,19 +1,50 @@
 let form;
-let allSlamArray=[];
+let allSlamArray = [];
 //Write code below to select the form element
 
-form = document.getElementsByTagName(form);
+form = document.querySelector(".form");
 // Write your code above
-console.log(form,"form");
+console.log(form, "form");
 
 // Add the evenlistner below
+const dataArray = [];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("I am inside submit");
 
+  const formData = new FormData(form);
+
+  for (const [name, value] of formData) {
+    allSlamArray.push({ name, value });
+  }
+  console.log(allSlamArray);
+  if (allSlamArray.length >= 11) {
+    allSlamArray.splice(0, 10);
+  }
+  form.querySelectorAll("input").forEach((input) => {
+    input.value = "";
+  });
+  showSlamList();
+});
 //Write your code above
 
-function showSlamList(){
-//Update the function here
+function showSlamList() {
+  //Update the function here
+  const div = document.querySelector(".allSlams");
 
-// Write your code above this line
+  const ul = document.createElement("ul");
+  const li = document.createElement("li");
+
+  allSlamArray.forEach(function (item) {
+    const li = document.createElement("li");
+    li.textContent = `${item.name}: ${item.value}`;
+    ul.appendChild(li);
+  });
+
+  ul.appendChild(li);
+
+  div.appendChild(ul);
+  // Write your code above this line
 }
 
-showSlamList();
+
