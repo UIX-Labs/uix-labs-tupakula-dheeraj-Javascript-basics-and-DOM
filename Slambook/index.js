46523a0b4d7940b2a81d051ab83d24b9293e1378
tupakula dheeraj <deerajtupakula@gmail.com>
2024-02-14 18:19:02 +0530

    task: Project Task-Adding a event listener

--- Slambook/index.js
+++ Slambook/index.js
@@ -24,27 +24,14 @@ form.addEventListener("submit", (e) => {
   form.querySelectorAll("input").forEach((input) => {
     input.value = "";
   });
-  showSlamList();
 });
 //Write your code above
 
 function showSlamList() {
   //Update the function here
-  const div = document.querySelector(".allSlams");
-
-  const ul = document.createElement("ul");
-  const li = document.createElement("li");
-
-  allSlamArray.forEach(function (item) {
-    const li = document.createElement("li");
-    li.textContent = `${item.name}: ${item.value}`;
-    ul.appendChild(li);
-  });
-
-  ul.appendChild(li);
-
-  div.appendChild(ul);
+ 
   // Write your code above this line
+  showSlamList();
 }
 
 
