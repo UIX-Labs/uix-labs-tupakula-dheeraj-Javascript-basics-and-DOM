cb61fc91b70c4f864183494b0fc2e7f594c6fc92
tupakula dheeraj <deerajtupakula@gmail.com>
2024-02-16 18:17:13 +0530

    task: Project Task-Adding elements in node

--- Slambook/index.js
+++ Slambook/index.js
@@ -12,39 +12,66 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("I am inside submit");
 
-  const formData = new FormData(form);
+ const contactName = form.querySelector('input[name="contactName"]');
+ const relationship = form.querySelector('input[name="relationship"]');
+ const likeInMe = form.querySelector('input[name="likeInMe"]');
+ const hateInMe = form.querySelector('input[name="hateInMe"]');
+ const deadReaction = form.querySelector('input[name="deadReaction"]');
+ const firstImpression = form.querySelector('input[name="firstImpression"]');
+ const beautifulMessage = form.querySelector('input[name="beautifulMessage"]');
+ const nickname = form.querySelector('input[name="nickname"]');
+ const songForMe = form.querySelector('input[name="songForMe"]');
+ const shareOpinion = form.querySelector('input[name="shareOpinion"]');
 
-  for (const [name, value] of formData) {
-    allSlamArray.push({ name, value });
+const entry = {};
+entry[contactName] = contactName.value;
+entry[relationship] = relationship.value;
+entry[likeInMe] = likeInMe.value;
+entry[hateInMe] = hateInMe.value;
+entry[deadReaction] = deadReaction.value;
+entry[firstImpression] = firstImpression.value;
+entry[beautifulMessage] = beautifulMessage.value;
+entry[nickname] = nickname.value;
+entry[songForMe] = songForMe.value;
+entry[shareOpinion] = shareOpinion.value;
+
+allSlamArray.splice(0, allSlamArray.length);
+
+ validateInput(contactName);
+ validateInput(relationship);
+ validateInput(likeInMe);
+ validateInput(hateInMe);
+ validateInput(deadReaction);
+ validateInput(firstImpression);
+ validateInput(beautifulMessage);
+ validateInput(nickname);
+ validateInput(songForMe);
+ validateInput(shareOpinion);
+
+
+allSlamArray.push(entry);
+
+
+  function validateInput(input) {
+    if(input.value.length === "") {
+        input.style.borderBottom = "2px solid red";
+        const errorMessage = document.createElement('span');
+        errorMessage.innerText = "*Error: This is required.";
+        input.after(errorMessage);
+        
+        return false;
+    }
+    return true;
   }
   console.log(allSlamArray);
-  if (allSlamArray.length >= 11) {
-    allSlamArray.splice(0, 10);
-  }
-  form.querySelectorAll("input").forEach((input) => {
-    input.value = "";
-  });
-  showSlamList();
 });
 //Write your code above
 
 function showSlamList() {
   //Update the function here
-  const div = document.querySelector(".allSlams");
-
-  const ul = document.createElement("ul");
-  const li = document.createElement("li");
-
-  allSlamArray.forEach(function (item) {
-    const li = document.createElement("li");
-    li.textContent = `${item.name}: ${item.value}`;
-    ul.appendChild(li);
-  });
-
-  ul.appendChild(li);
-
-  div.appendChild(ul);
+  
   // Write your code above this line
 }
+  showSlamList();
 
 
